f0d5ca541e6ad69fc30ea420d7099cead909a6f7
Vadim <maryushkin.vadim@gmail.com>
2023-12-27 13:42:22 +0300

    Second commit

---
+++ WpfAppItProger/AppDBContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using WpfAppItProger.Models;
+
+namespace WpfAppItProger
+{
+    internal class AppDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppDBContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySql("Server=localHost; Database=modul_14; Port=3306; User=root; Password=root");
+        }
+    }
+}
